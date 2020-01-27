02e29f06e0af03461e2979881282e59f803454f3
kpcruz <kelvinmembership@gmail.com>
2020-01-27 15:34:32 -0800

    added all relevant api functions from OMDB. Havent figured out the required search parameters for tvEpisodes yet, but everything else works

--- apis/omdb.js
+++ apis/omdb.js
@@ -2,14 +2,41 @@ import request from "request";
 import { checkForError } from "../utils";
 
 const omdb = {
-	queryUrl: function(userInput) {
+	queryMovieTitle: function(userInput) {
 		return (
 			"http://www.omdbapi.com/?t=" +
 			userInput.search +
-			"&y=&plot=short&apikey=trilogy"
+			"&y=&plot=full&apikey=trilogy"
 		);
 	},
-	logResults: function(body) {
+	queryIMDBId: function(userInput) {
+		return (
+			"http://www.omdbapi.com/?i=" +
+			userInput.search +
+			"&y=&plot=full&apikey=trilogy"
+		);
+	},
+	queryTVSeries: function(userInput) {
+		return (
+			"http://www.omdbapi.com/?s=" +
+			userInput.search +
+			"&type=series&apikey=trilogy"
+		);
+	},
+	queryTVEpisode: function(userInput) {
+		return (
+			"http://www.omdbapi.com/?s=" +
+			userInput.search +
+			"&type=episode&apikey=trilogy"
+		);
+	},
+	logTVSeriesResults: function() {
+		console.log(JSON.parse(body).Search[0]);
+	},
+	logTVEpisodeResults: function(body) {
+		console.log(JSON.parse(body));
+	},
+	logMovieResults: function(body) {
 		console.log("Title: " + JSON.parse(body).Title);
 		console.log("Release Year: " + JSON.parse(body).Year);
 		console.log("IMBD Rating: " + JSON.parse(body).imdbRating);
@@ -20,11 +47,11 @@ const omdb = {
 	},
 	runQuery: function(error, response, body) {
 		if (!error && response.statusCode === 200) {
-			omdb.logResults(body);
+			omdb.logMovieResults(body);
 		}
 	},
 	searchMovieTitle: function(userInput) {
-		request(omdb.queryUrl(userInput), function(error, response, body) {
+		request(omdb.queryMovieTitle(userInput), function(error, response, body) {
 			checkForError(error);
 			omdb.runQuery(error, response, body);
 		});
